d629ace2b6223a05b3ef8705da30ff46100d1fb9
Jaewoo Nam <namssabu12@gmail.com>
2020-03-21 15:23:41 +0900

    add test

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 const express = require('express')
 const app = express()
+const morgan = require('morgan')
+const users = [
+    {id: 1, name: 'alice'},
+    {id: 2, name: 'bek'},
+    {id: 3, name: 'chris'},
+]
+
+app.use(morgan('dev'))
+
 const port = 3000
 
 // set routing
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/users', (req, res) => {
+    res.json(users)
+})
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`)) // when listen completed -> call back
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}!`)
+}) // when listen completed -> call back
