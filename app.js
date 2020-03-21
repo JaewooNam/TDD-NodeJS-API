3d80014dcf3ff9f47128fa984f4946e2946513ad
Jaewoo Nam <namssabu12@gmail.com>
2020-03-21 14:09:08 +0900

    Express practice

--- app.js
+++ app.js
@@ -1,22 +1,6 @@
 const express = require('express');
-const morgan = require('morgan');
 const app = express();
 
-// middleware
-function logger(req, res, next) {
-    console.log('I am logger');
-    next(); // for next processing
-}
-
-function logger2(req, res, next) {
-    console.log('I am logger2');
-    next(); // for next processing
-}
-
-// Add middleware
-app.use(logger);
-app.use(logger2);
-app.use(morgan('dev'));
 
 app.listen(3000, () => {
     console.log('Server is running');
--- index.js
+++ index.js
@@ -1,23 +1,8 @@
-const http = require('http'); // get http node module
+const express = require('express')
+const app = express()
+const port = 3000
 
-const hostname = '127.0.0.1';
-const port = 3000;
+// set routing
+app.get('/', (req, res) => res.send('Hello World!'))
 
-const server = http.createServer((req, res) => {
-    if (req.url === '/') {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/plain');
-        res.end('Hello World\n');
-    } else {
-        res.statusCode = 404;
-        res.end('Nod Found');
-    }
-});
-
-// make server waiting state
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-});
-
-// curl -X GET 'localhost:3000/users'
-// curl -X GET 'localhost:3000/users -v'
+app.listen(port, () => console.log(`Example app listening on port ${port}!`)) // when listen completed -> call back
